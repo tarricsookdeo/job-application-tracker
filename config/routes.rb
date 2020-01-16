2babd93f6661ae563637e39a571bc2382d4c4a37
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-01-15 19:13:18 -0500

    Will config routes to add omniauth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'application#welcome'
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
 end
