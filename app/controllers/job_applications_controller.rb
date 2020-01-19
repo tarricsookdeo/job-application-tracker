74a7b9940a9a89c80e15fc221573d459f1912b2a
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-01-19 06:01:50 -0500

    Added show view and controller method to job_applications.

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -3,5 +3,9 @@ class JobApplicationsController < ApplicationController
     def index
         @applications = JobApplication.where(user_id: current_user.id)
     end
-    
+
+    def show
+        @application = JobApplication.find_by(id: params[:id])
+    end
+
 end
