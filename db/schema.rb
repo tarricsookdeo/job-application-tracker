1e59e714b7a123be226fc65f12ebdd88ad79b15c
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-01-19 04:21:48 -0500

    Will add company and user to job_application.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_090159) do
+ActiveRecord::Schema.define(version: 2020_01_19_091944) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2020_01_19_090159) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_job_applications_on_company_id"
+    t.index ["user_id"], name: "index_job_applications_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +42,6 @@ ActiveRecord::Schema.define(version: 2020_01_19_090159) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "job_applications", "companies"
+  add_foreign_key "job_applications", "users"
 end
