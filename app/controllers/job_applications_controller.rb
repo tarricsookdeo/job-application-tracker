928f228c22668b95d5a003aaef5e5bb5563cd6a8
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-01-20 03:39:48 -0500

    Added ability to add a new application with proper associations.

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -8,4 +8,33 @@ class JobApplicationsController < ApplicationController
         @application = JobApplication.find_by(id: params[:id])
     end
 
+    def new
+        @application = current_user.job_applications.build
+        @application.build_company
+    end
+
+    def create
+        @application = JobApplication.new(job_application_params)
+        @application.user_id = current_user.id
+
+        if @application.save
+            redirect_to job_applications_path
+        else
+            flash.now[:error] = @application.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+
+    def job_application_params
+        params.require(:job_application).permit(
+            :user_id,
+            :company_id,
+            :status,
+            company_attributes: [
+                :name
+            ]
+        )
+    end
 end
