b7e892ae819edf8fdddc3d462be0d6df8acecc0b
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-01-21 17:58:56 -0500

    Will add nested routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   root to: 'application#welcome'
-  resources :job_applications, only: [:new, :create, :index, :update, :destroy, :show, :edit]
-
+  
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
-
+  
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
+  end
+  
+  resources :job_applications, only: [:new, :create, :index, :update, :destroy, :show, :edit]
+
+  resources :companies do
+    resources :job_applications, only: [:index]
   end
 
 end
